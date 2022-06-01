e1e39bd8903c076090884964bf4672696b1e9004
rhuakela <uthumakela8724@gmail.com>
2022-06-01 13:49:55 +0530

    This is first commit

---
+++ lib/main.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+//The main function is the starting point for all our flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+              'NSBM Green University',
+            style: TextStyle(
+              color: Colors.green,
+            ),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/uthum.jpg'),
+              ),
+              Text(
+                'R.H Uthum Akela',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Undergraduate (computer Science)',
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                 children: <Widget>[
+                  Icon(Icons.phone_android,
+                    color: Colors.lightBlueAccent,
+                  ),
+                   SizedBox(
+                     width: 10.0,
+                   ),
+                   Text('+94-703142188',
+                     style: TextStyle(
+                       color: Colors.white,
+                       fontWeight: FontWeight.bold,
+                     ),
+                   ),
+                 ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                   Icon(Icons.email_outlined,
+                    color: Colors.lightBlueAccent,
+                   ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('uthumakela8724@gmail.com',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.email_outlined,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('rhuakela@gmail.com',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.alternate_email_outlined,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('rhuakela@students.nsbm.ac.lk',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.alternate_email_outlined,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('10749839@students.plymouth.ac.uk',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.contact_mail,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('NSBM - xxxxx',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.contact_mail,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('Plymouth - xxxxxxxx',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
